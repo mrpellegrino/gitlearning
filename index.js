42be51cfc4092357fa14241ffdf613909d8904d4
Dayvson <dayvson.pellegrinorodrigues@gmail.com>
2021-01-01 13:49:55 -0300

    Commit Inicial

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/teste', (req, res) => {
+    return res.json({"message":"Olá Mundo"})
+});
+
+app.listen(3333, () => {
+    console.log('App listening on port 3333!');
+});
